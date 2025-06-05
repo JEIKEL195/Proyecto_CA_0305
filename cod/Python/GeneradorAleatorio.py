bfec61378d11f2e75cc8c0d694cff90b4b51028f
Jeikel Navarro <jeikel.navarro@ucr.ac.cr>
2025-06-05 00:00:04 -0600

    feat: se agrega método que grafica la frecuencia de los números aleatorios

--- cod/Python/GeneradorAleatorio.py
+++ cod/Python/GeneradorAleatorio.py
@@ -7,6 +7,7 @@ Created on Wed Jun  4 23:32:52 2025
 
 import numpy as np
 import secrets
+import matplotlib as plt
 
 class GeneradorAleatorio:
     def __init__(self):
@@ -18,3 +19,21 @@ class GeneradorAleatorio:
 
     def entero(self, minimo, maximo):
         return self.__rng.integers(minimo, maximo + 1)
+    
+    def vector_enteros(self, minimo, maximo, n):
+        return self.rng.integers(minimo, maximo + 1, size=n)
+
+    def graficar_frecuencia_enteros(self, minimo, maximo, n=1000):
+        """Genera n números enteros en [minimo, maximo] y grafica su frecuencia."""
+        datos = self.vector_enteros(minimo, maximo, n)
+        valores, frecuencias = np.unique(datos, return_counts=True)
+
+        plt.figure(figsize=(12, 6))
+        plt.bar(valores, frecuencias, color='mediumseagreen', edgecolor='black')
+        plt.title(f"Frecuencia de {n} números aleatorios en [{minimo}, {maximo}]")
+        plt.xlabel("Número")
+        plt.ylabel("Frecuencia")
+        plt.xticks(np.arange(minimo, maximo + 1))
+        plt.grid(axis='y', linestyle='--', alpha=0.6)
+        plt.tight_layout()
+        plt.show()
