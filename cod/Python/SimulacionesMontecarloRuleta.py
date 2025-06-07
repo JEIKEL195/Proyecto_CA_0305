dfaa37fe61f8ba34cfffc143cb24eb1bc451c75b
Jeikel Navarro <jeikel.navarro@ucr.ac.cr>
2025-06-07 04:08:19 -0600

    feat: se agrega estrategia martingala para la ruleta

--- cod/Python/SimulacionesMontecarloRuleta.py
+++ cod/Python/SimulacionesMontecarloRuleta.py
@@ -82,6 +82,81 @@ class SimulacionesMontecarloRuleta:
         
         return self.__resultados
     
+    def simular_martingala(self, tipo_apuesta, valor_apuesta, monto_inicial):
+        for simulacion in range(self.__ctd_simulaciones):
+            ruleta = Ruleta()
+            jugador = JugadorRuleta(self.__jugador_original.nombre, self.__jugador_original.saldo)
+            monto = monto_inicial
+    
+            for _ in range(self.__ctd_jugadas):
+                if monto > jugador.saldo:
+                    break
+    
+                jugador.hacer_apuesta(tipo_apuesta, valor_apuesta, monto)
+                resultado = ruleta.girar()
+                ganador = jugador.actualizar_saldo(resultado)
+    
+                if ganador:
+                    monto = monto_inicial
+                    
+                else:
+                    monto *= 2
+                    
+            self.__historiales.append(jugador.get_historial())
+            self.__resultados.append(jugador.saldo)
+            
+        return self.__resultados
+    
+    def martingala_hasta_objetivo(self, tipo_apuesta: str, valor_apuesta, monto_inicial: float, factor_objetivo: float):
+        '''Simula la estrategia Martingala hasta alcanzar el objetivo de capital o quedar sin saldo.'''
+    
+        saldo_objetivo = self.__jugador_original.saldo * factor_objetivo
+        historial_tiempo = []
+        exitos = 0  # contar cuántas veces se alcanza el objetivo
+    
+        for simulacion in range(self.__ctd_simulaciones):
+            ruleta = Ruleta()
+            jugador = JugadorRuleta(self.__jugador_original.nombre, self.__jugador_original.saldo)
+            monto = monto_inicial
+            historial = [jugador.saldo]
+            tiempo = 0
+    
+            while 0 < jugador.saldo < saldo_objetivo:
+                if monto > jugador.saldo:
+                    break  # no puede apostar más
+    
+                jugador.hacer_apuesta(tipo_apuesta, valor_apuesta, monto)
+                resultado = ruleta.girar()
+                fue_ganador = jugador.actualizar_saldo(resultado)
+                historial.append(jugador.saldo)
+    
+                if fue_ganador:
+                    monto = monto_inicial
+                else:
+                    monto *= 2
+                    
+                tiempo += 1
+    
+            if jugador.saldo >= saldo_objetivo:
+                exitos += 1
+    
+            self.__historiales.append(historial)
+            self.__resultados.append(jugador.saldo)
+            historial_tiempo.append(tiempo)
+    
+        probabilidad_exito = exitos / self.__ctd_simulaciones
+        
+        tiempo_promedio = sum(historial_tiempo)/self.__ctd_simulaciones
+        
+        return {
+            'Éxitos': exitos,
+            'Fracaso': self.__ctd_simulaciones - exitos,
+            'Probabilidad_exito': probabilidad_exito,
+            'Tiempo_promedio': tiempo_promedio
+        }
+
+    
+
     
     def trayectorias(self):
         ''' Grafica distintas trayectorias del juego de ruleta.
@@ -126,8 +201,11 @@ class SimulacionesMontecarloRuleta:
         plt.tight_layout()
         plt.show()
     
-jugador = JugadorRuleta(nombre = "Venegas", saldo_inicial = 120000)
-sim = SimulacionesMontecarloRuleta(jugador, 10000, 500)
-sim.simular('paridad', 'par', 10000)
-
-sim.graficar_probabilidad(0.75, [10, 100, 1000, 10_000])
+jugador = JugadorRuleta(nombre = "Venegas", saldo_inicial = 100)
+sim = SimulacionesMontecarloRuleta(jugador, 10000, 5000)
+#sim.simular_martingala('paridad', 'par', 1000)
+#sim.simular_martingala_hasta_objetivo('paridad', 'par', 10, 2)
+sim.martingala_inversa_hasta_objetivo('paridad', 'par', 10, 1.1)
+#sim.graficar_probabilidad(2, [10, 100, 1000, 10_000])
+#sim.estadisticas()
+#sim.trayectorias()
